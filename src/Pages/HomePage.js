c1fd4360d13a93ca93da950c584283a287b0b068
masnoonJunaid <masnoon20@gmail.com>
2022-10-02 13:55:18 +0530

    homepage composition

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,26 @@
 import { HeadingComponent } from "../components/HeadingComponent";
 import { EventCard } from "../components/EventCard";
 import { DynamicButton } from "../components/Button";
+import buttonStyles from '../assets/styles/homeButton.module.scss'
+import { GiPartyPopper } from 'react-icons/gi'
+import homePageStyle from '../assets/styles/Homepage.module.css';
 
 
 export const HomePage = () => {
-  return <div>
-    <HeadingComponent />
-    <EventCard />
-    <DynamicButton />
-  </div>
+  return(
+    <div className={ homePageStyle.homeContainer }>
+      <HeadingComponent />
+      <div className={homePageStyle.eventContainer}>
+        <EventCard />
+      </div>
+      <div className={homePageStyle.buttonContainer}>
+        <DynamicButton
+          styles={buttonStyles}
+          ButtonIcon = {GiPartyPopper}
+          buttonText={"Create my event"}
+          buttonLink="/create-new-event"
+        />
+      </div>
+    </div>
+)
 }
